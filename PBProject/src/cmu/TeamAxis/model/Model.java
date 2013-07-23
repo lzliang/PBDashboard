4f324b936da34f280c372ee5f5fe56cf52e16fd4
Abdullah Al Mahmood <sajibcseofkuet@gmail.com>
2013-07-22 23:31:26 -0400

    Model modified

--- PBProject/src/cmu/TeamAxis/model/Model.java
+++ PBProject/src/cmu/TeamAxis/model/Model.java
@@ -4,12 +4,48 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
 public class Model {
-	private ItemDAO itemDAO = new ItemDAO();
-	private UserDAO userDAO = new UserDAO();
+    private final ItemDAO itemDAO = new ItemDAO();
+    private final UserDAO userDAO = new UserDAO();
+    private final CustomerDAO customerDAO = new CustomerDAO();
+    private final EmployeeDAO employeeDAO = new EmployeeDAO();
+    private final OtherPricesDAO otherPricesDAO = new OtherPricesDAO();
+    private final ProductDAO productDAO = new ProductDAO();
+    private final ProductLocationDAO productLocationDAO = new ProductLocationDAO();
+    private final RequestDAO requestDAO = new RequestDAO();
 
-	public Model(ServletConfig config) throws ServletException {
-	}
-	
-	public ItemDAO getItemDAO()  { return itemDAO; }
-	public UserDAO getUserDAO()  { return userDAO; }
+    public Model(ServletConfig config) throws ServletException {
+    }
+
+    public ItemDAO getItemDAO() {
+	return itemDAO;
+    }
+
+    public UserDAO getUserDAO() {
+	return userDAO;
+    }
+
+    public CustomerDAO getCustomerDAO() {
+	return customerDAO;
+    }
+
+    public EmployeeDAO getEmployeeDAO() {
+	return employeeDAO;
+    }
+
+    public OtherPricesDAO getOtherPricesDAO() {
+	return otherPricesDAO;
+    }
+
+    public ProductLocationDAO getProductLocationDAO() {
+	return productLocationDAO;
+    }
+
+    public ProductDAO getProductDAO() {
+	return productDAO;
+    }
+
+    public RequestDAO getRequestDAO() {
+	return requestDAO;
+    }
+
 }

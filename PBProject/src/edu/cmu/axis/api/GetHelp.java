8e62b0eea6ed342bec0e1ee646e285ba65e84a5f
Jingwei <jingwei.hao@gmail.com>
2013-07-26 14:30:05 -0400

    fix web.xml

--- PBProject/src/edu/cmu/axis/api/GetHelp.java
+++ PBProject/src/edu/cmu/axis/api/GetHelp.java
@@ -1,16 +1,11 @@
 package edu.cmu.axis.api;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import com.google.appengine.labs.repackaged.org.json.JSONException;
-import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 @Path("/help")
 public class GetHelp {

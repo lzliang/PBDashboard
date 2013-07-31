36e9c9323d2fc07aa60b008dfda517b74adec1d5
Jingwei <jingwei.hao@gmail.com>
2013-07-31 10:29:22 -0400

    rest api defined

--- PBProject/src/edu/cmu/axis/api/Feedback.java
+++ PBProject/src/edu/cmu/axis/api/Feedback.java
@@ -1,18 +1,35 @@
 package edu.cmu.axis.api;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.mortbay.util.ajax.JSON;
+
+import com.google.gson.Gson;
+
 
 @Path("/feedback")
 public class Feedback {
 
 	@POST
 	@Produces(MediaType.TEXT_PLAIN)
-	public String updateFeedback(String inputRequest){
+	public String updateFeedback(String inputRequest) {
 		//do something with gson TODO
+		Gson gson = new Gson();
+		Feedback fb = gson.fromJson(inputRequest, Feedback.class);
+		Map<String,String> returnmap = new HashMap<String,String>();
+		//if(saveFeedback(fb)){
+			returnmap.put("status","success");
+		//}else{
+			returnmap.put("status","error");
+		//}
 		
-		return "";
+		return JSON.toString(returnmap);
 	}
 }
--- PBProject/src/edu/cmu/axis/api/GetHelp.java
+++ PBProject/src/edu/cmu/axis/api/GetHelp.java
@@ -1,5 +1,7 @@
 package edu.cmu.axis.api;
 
+import java.util.HashMap;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -8,20 +10,25 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.datanucleus.sco.backed.Map;
+
+import com.google.gson.Gson;
+
 @Path("/help")
 public class GetHelp {
 
-	/**
-	 * This is an example of GET
-	 * The url of this method is: 
-	 * http://localhost:8888/_api/help, method get
-	 * @return a json string
-	 */
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String info() {
-		return "Hello Jersey on Google App Engine";
-	}
+//	/**
+//	 * This is an example of GET
+//	 * The url of this method is: 
+//	 * http://localhost:8888/_api/help, method get
+//	 * @return a json string
+//	 */
+//	@GET
+//	@Path("{machineID}")
+//	@Produces(MediaType.TEXT_PLAIN)
+//	public String getHelpingEmployee() {
+//		return "Hello Jersey on Google App Engine";
+//	}
 
 	/**
 	 * The url of this method is: 
@@ -32,8 +39,14 @@ public class GetHelp {
 	@POST
 	//@Path("/")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String processRequset(String pData) {
-	    //Use gson to convert input string into maps would do all our work
+	public String helpRequest(String pData) {
+	    Gson gson = new Gson();
+	    HashMap<String, String> request = gson.fromJson(pData, HashMap.class);
+	   // if(saveHelpRequest(request)){ //TODO
+	    	
+	   // }else{
+	    	
+	  //  }
 	    return pData;
 	}
 	
@@ -47,8 +60,8 @@ public class GetHelp {
 	@GET
 	@Path("{machineID}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getHelpInfo(@PathParam("machineID") int machineID) {
-	//public String getinfo() {
+	public String getHelpingEmployee(@PathParam("machineID") String machineID) {
+	//	Map<String, String> rt = getHelpingEmployee(machineID);
 		return machineID+"";
 	}
 }

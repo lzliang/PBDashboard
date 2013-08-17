665a22a0b503e188e8f824756e95bea8b00b9ecb
Jingwei <jingwei.hao@gmail.com>
2013-08-17 13:16:09 -0400

    fix sort day and deployment version

--- PBProject/src/edu/cmu/axis/api/Stats.java
+++ PBProject/src/edu/cmu/axis/api/Stats.java
@@ -23,38 +23,43 @@ import com.google.gson.Gson;
 public class Stats {
 	static Gson gson = new Gson();
 	RequestDAO rd = new RequestDAO();
-	private final static Logger LOGGER = Logger.getLogger(Feedback.class.getName());
+	private final static Logger LOGGER = Logger.getLogger(Feedback.class
+			.getName());
+
 	@GET
 	@Path("/servno/{intvl}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response getInfo(@PathParam("intvl")  String intvl) {
-		
+	public Response getInfo(@PathParam("intvl") String intvl) {
+
 		Map<String, Object> rt = new HashMap<String, Object>();
-		Map<String,Integer> stats;
-		try{
+		Map<String, Integer> stats;
+		try {
 			stats = rd.getRequestStats();
-		}
-		catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			return Util.returnError(null, e);
 		}
 		LOGGER.severe("RequestStats got from DAO: " + gson.toJson(stats));
 		List<Long[]> statsList = new ArrayList<Long[]>();
-		if(intvl.trim().toLowerCase().equals("all")){
-			String[] keys = (String[])stats.keySet().toArray();
+		if (intvl.trim().toLowerCase().equals("all")) {
+			Object[] objKeys = stats.keySet().toArray();
+			String[] keys = new String[objKeys.length];
+			for (int i = 0; i < keys.length; i++) {
+				keys[i] = (String) objKeys[i];
+			}
 			Arrays.sort(keys);
-			LOGGER.severe("Sorted keys: "+ Arrays.toString(keys));
-			for(String day : keys){
+			LOGGER.severe("Sorted keys: " + Arrays.toString(keys));
+			for (String day : keys) {
 				Long[] curr = new Long[2];
 				curr[0] = Long.parseLong(day);
-				curr[1] = (long)(stats.get(day));
+				curr[1] = (long) (stats.get(day));
 				statsList.add(curr);
 			}
-			rt.put("data",statsList);
+			rt.put("data", statsList);
 		}
 		rt.put("status", "success");
-		return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(gson.toJson(rt)).build();
+		return Response.status(200).header("Access-Control-Allow-Origin", "*")
+				.entity(gson.toJson(rt)).build();
 	}
-	
 
 }

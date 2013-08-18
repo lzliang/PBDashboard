4d55a3cbc4a5ac49f8a948c90eb68aaf58a8b108
Abdullah Al Mahmood <sajibcseofkuet@gmail.com>
2013-08-18 17:18:04 -0400

    AmazonProductsDAO - I am not in full sense, so it might be incomplete, review it and let me know if I need to implement a few more methods

---
+++ PBProject/src/cmu/axis/model/AmazonProductsDAO.java
@@ -0,0 +1,134 @@
+package cmu.axis.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cmu.axis.databean.AmazonProducts;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Transaction;
+
+public class AmazonProductsDAO {
+    private final DatastoreService datastore = DatastoreServiceFactory
+	    .getDatastoreService();
+    private final Key rootKey = KeyFactory.createKey("Root", "root");
+    private final Query ascendingQuery = new Query("AmazonProduct", rootKey)
+	    .addSort("barCode", Query.SortDirection.ASCENDING);
+
+    public void addProduct(AmazonProducts product) throws DAOException {
+	Transaction t = null;
+	try {
+	    t = datastore.beginTransaction();
+
+	    Entity e = new Entity("AmazonProduct", rootKey);
+	    // e.setProperty("customerId", customer.getCustomerId());
+	    e.setProperty("productName", product.getProductName());
+	    e.setProperty("review", product.getReview());
+	    e.setProperty("productDescription", product.getProductDescription());
+	    e.setProperty("barcode", product.getBarCode());
+	    e.setProperty("similarProducts", product.getSimilarProducts());
+
+	    // Create a new entity in the datastore. Id will be automatically
+	    // set.
+	    datastore.put(e);
+	    product.setProductID(e.getKey().getId());
+	    // item.setId(e.getKey().getId()); id stuff, add later
+
+	    t.commit();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new DAOException(e);
+	} finally {
+	    if (t != null && t.isActive())
+		t.rollback();
+	}
+    }
+
+    public AmazonProducts[] getProducts() throws DAOException {
+	try {
+	    List<AmazonProducts> products = runAscendingQuery();
+	    return products.toArray(new AmazonProducts[products.size()]);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new DAOException(e);
+	}
+    }
+
+    public AmazonProducts getProduct(String barcode) throws DAOException,
+	    EntityNotFoundException {
+	try {
+	    AmazonProducts aProduct = new AmazonProducts();
+	    List<AmazonProducts> products = runAscendingQuery();
+	    for (AmazonProducts product : products) {
+		if (product.getBarCode().equals(barcode))
+		    aProduct = product;
+	    }
+	    return aProduct;
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new DAOException(e);
+	}
+    }
+
+    public boolean doesExist(String barcode) throws DAOException,
+	    EntityNotFoundException {
+	AmazonProducts[] allProducts = getProducts();
+	for (int i = 0; i < allProducts.length; i++) {
+	    if (allProducts[i].getBarCode().equalsIgnoreCase(barcode))
+		return true;
+	}
+	return false;
+
+    }
+
+    public void updateProduct(long pID, AmazonProducts pBean)
+	    throws DAOException, EntityNotFoundException {
+	Key pKey = KeyFactory.createKey(rootKey, "AmazonProduct", pID);
+	Entity product = datastore.get(pKey);
+	product.setProperty("barcode", pBean.getBarCode());
+	product.setProperty("productName", pBean.getProductName());
+	product.setProperty("review", pBean.getReview());
+	product.setProperty("productDescription", pBean.getProductDescription());
+	product.setProperty("barcode", pBean.getBarCode());
+	product.setProperty("similarProducts", pBean.getSimilarProducts());
+	datastore.put(product);
+    }
+
+    public AmazonProducts getProduct(long productID) throws DAOException,
+	    EntityNotFoundException {
+	Key pKey = KeyFactory.createKey(rootKey, "AmazonProduct", productID);
+	Entity product = datastore.get(pKey);
+	AmazonProducts pBean = makeBean(product);
+	return pBean;
+    }
+
+    private List<AmazonProducts> runAscendingQuery() {
+	List<Entity> entities = datastore.prepare(ascendingQuery).asList(
+		FetchOptions.Builder.withLimit(100));
+	List<AmazonProducts> products = new ArrayList<AmazonProducts>();
+	for (Entity e : entities) {
+	    products.add(makeBean(e));
+	}
+	return products;
+    }
+
+    private AmazonProducts makeBean(Entity e) {
+	AmazonProducts pbean = new AmazonProducts();
+	pbean.setProductID(e.getKey().getId());
+	pbean.setProductName((String) e.getProperty("productName"));
+	pbean.setReview((String) e.getProperty("review"));
+	pbean.setProductDescription((String) e
+		.getProperty("productDescription"));
+	pbean.setBarCode((String) e.getProperty("barcode"));
+	pbean.setSimilarProducts((String) (e.getProperty("similarProducts")));
+	return pbean;
+    }
+
+}
